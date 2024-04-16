f89dcad76cbb5127e6e808fac53f8493d72f48f7
John Mwangi <mail.mwangi@gmail.com>
2024-04-16 23:50:36 +0300

    Fix cost display

--- app_lang.py
+++ app_lang.py
@@ -5,10 +5,22 @@ from dotenv import load_dotenv
 from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
 
+from src import utils
 from src.params import models
 from src.prompt_lang import memory, prompt_template
 
 load_dotenv()
+
+
+def display_cost(tokens, prompt_cost, conv_cost):
+    return f"""
+    <p style="font-size:12px;text-align:right;">
+    Tokens: {tokens}<br>
+    Prompt cost: {prompt_cost}<br>
+    Conversation cost: {conv_cost}
+    </p>
+    """
+
 
 # Side bar
 st.sidebar.title("ChatGPT API Interface")
@@ -41,3 +53,33 @@ if prompt:
 
     with st.chat_message(name="assistant"):
         st.markdown(response)
+
+    # Cost calculation
+    input_tokens = utils.num_tokens_from_string(message=prompt, model=model)
+    output_tokens = utils.num_tokens_from_string(message=response, model=model)
+
+    token_used, prompt_cost = utils.calc_prompt_cost(
+        input_tokens, output_tokens, model
+    )
+    conversation_cost = utils.calc_conversation_cost(
+        prompt_cost=prompt_cost, new_conversation=True
+    )
+
+    result = {}
+    result["msg"] = response
+    result["token_used"] = token_used
+    result["prompt_cost"] = prompt_cost
+    result["conversation_cost"] = conversation_cost
+
+    token_used = result.get("token_used")
+    prompt_cost = result.get("prompt_cost")
+    conversation_cost = result.get("conversation_cost")
+
+    st.write(
+        display_cost(
+            tokens=token_used,
+            prompt_cost=prompt_cost,
+            conv_cost=conversation_cost,
+        ),
+        unsafe_allow_html=True,
+    )
