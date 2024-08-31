580a0c11aaabaed7b604494ffdc1fb928e26815e
John Mwangi <mail.mwangi@gmail.com>
2024-08-31 11:44:14 +0300

    Use pydantic for settings

--- chatgpt_ui/configs/params.py
+++ chatgpt_ui/configs/params.py
@@ -1,21 +1,11 @@
 from pathlib import Path
 
-models = ["gpt-4-1106-preview", "gpt-3.5-turbo-1106"]
-CHATGPT_ROLE = "You are an expert Python programmer."
-model_pricing = {
-    "gpt-4-1106-preview": {
-        "input_cost_usd_per_1K_tokens": 0.01,
-        "output_cost_usd_per_1K_tokens": 0.03,
-    },
-    "gpt-3.5-turbo-1106": {
-        "input_cost_usd_per_1K_tokens": 0.0010,
-        "output_cost_usd_per_1K_tokens": 0.0020,
-    },
-}
+import yaml
+from pydantic_settings import BaseSettings
 
-ROOT_DIR = Path(__file__).parent.parent.resolve()
-costs_path = ROOT_DIR / "files/costs.pkl"
-msgs_path = ROOT_DIR / "files/messages.pkl"
+PKG_DIR = Path(__file__).parent.parent.resolve()
+
+CHATGPT_ROLE = "You are an expert Python programmer."
 
 template = (
     CHATGPT_ROLE
@@ -25,3 +15,20 @@ template = (
     assistant:
     """
 )
+
+
+class Settings(BaseSettings):
+    models: list
+    pricing: dict
+
+    def load():
+        settings_path = PKG_DIR / "configs/settings.yaml"
+        with open(settings_path, mode="r") as f:
+            settings = yaml.safe_load(f)
+
+        return Settings(**settings)
+
+
+if __name__ == "__main__":
+    print(Settings.load().models)
+    print(Settings.load().pricing)
